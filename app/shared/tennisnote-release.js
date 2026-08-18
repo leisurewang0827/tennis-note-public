9e7645c8e15db1c9ee4ca155ccf94d23c6ff76e8
leisurewang0827 <leisurewang0827@gmail.com>
2026-08-18 15:44:26 +0900

    Publish Tennis Note PWA 1.0.362

--- app/shared/tennisnote-release.js
+++ app/shared/tennisnote-release.js
@@ -1,9 +1,9 @@
 (function () {
   const release = Object.freeze({
-    version: "1.0.361",
-    releaseId: "2026.08.18.06",
-    appSurfaceVersion: "1.0.361",
-    deployedAt: "2026-08-18T15:04:35+09:00",
+    version: "1.0.362",
+    releaseId: "2026.08.18.07",
+    appSurfaceVersion: "1.0.362",
+    deployedAt: "2026-08-18T15:32:29+09:00",
     minimumNativeShellVersion: "1.0.118",
     nativeShell: {
       version: "1.0.359",
