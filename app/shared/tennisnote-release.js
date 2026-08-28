3f4370abaa7ac314e70ce7dca2c53ab2027e1107
leisurewang0827 <leisurewang0827@gmail.com>
2026-08-29 03:04:40 +0900

    chore: 1.0.428

--- app/shared/tennisnote-release.js
+++ app/shared/tennisnote-release.js
@@ -1,9 +1,9 @@
 (function () {
   const release = Object.freeze({
-    version: "1.0.427",
-    releaseId: "2026.08.29.01",
-    appSurfaceVersion: "1.0.427",
-    deployedAt: "2026-08-29T02:30:26+09:00",
+    version: "1.0.428",
+    releaseId: "2026.08.29.02",
+    appSurfaceVersion: "1.0.428",
+    deployedAt: "2026-08-29T03:04:12+09:00",
     minimumNativeShellVersion: "1.0.118",
     nativeShell: {
       version: "1.0.426",
--- app/tennis-note-member-app/forms/common.js
+++ app/tennis-note-member-app/forms/common.js
@@ -26,7 +26,7 @@ function registerPwaInstallPrompt() {
 function registerPwaServiceWorker() {
   window.TennisNoteReleaseUpdater?.start({
     manifestUrl: "../release.json",
-    workerUrl: "./service-worker.js?v=1.0.427",
+    workerUrl: "./service-worker.js?v=1.0.428",
     remoteAppUrl: "https://tennisnote-app.pages.dev/",
   });
 }
