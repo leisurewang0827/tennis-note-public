09b70d518b284366531839768318781f8a4bf745
leisurewang0827 <leisurewang0827@gmail.com>
2026-09-02 15:01:19 +0900

    chore(tennisnote): release dev 1.0.463

--- app/tennis-note-member-app/forms/common.js
+++ app/tennis-note-member-app/forms/common.js
@@ -45,7 +45,7 @@ function registerPwaInstallPrompt() {
 function registerPwaServiceWorker() {
   window.TennisNoteReleaseUpdater?.start({
     manifestUrl: "../release.json",
-    workerUrl: "./service-worker.js?v=1.0.462",
+    workerUrl: "./service-worker.js?v=1.0.463",
     remoteAppUrl: "https://tennisnote-app.pages.dev/",
   });
 }
