79a31fbbd9d0d35e84d2b4d11eddf13da587b968
leisurewang0827 <leisurewang0827@gmail.com>
2026-09-12 00:30:06 +0900

    chore: 1.0.493

--- app/shared/tennisnote-release.js
+++ app/shared/tennisnote-release.js
@@ -1,9 +1,9 @@
 (function () {
   const release = Object.freeze({
-    version: "1.0.492",
-    releaseId: "2026.09.12.01",
-    appSurfaceVersion: "1.0.492",
-    deployedAt: "2026-09-12T00:07:28+09:00",
+    version: "1.0.493",
+    releaseId: "2026.09.12.02",
+    appSurfaceVersion: "1.0.493",
+    deployedAt: "2026-09-12T00:30:01+09:00",
     minimumNativeShellVersion: "1.0.118",
     nativeShell: {
       version: "1.0.428",
