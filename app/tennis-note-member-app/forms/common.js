bc94298fbff60a24354e42cd28d5d461d4e0cf95
leisurewang0827 <leisurewang0827@gmail.com>
2026-09-12 00:32:56 +0900

    chore: 1.0.493

--- app/tennis-note-member-app/forms/common.js
+++ app/tennis-note-member-app/forms/common.js
@@ -46,7 +46,7 @@ function registerPwaServiceWorker() {
   const memberPortal = window.TennisNoteRuntimeEnvironment?.resolvePortal?.("member");
   window.TennisNoteReleaseUpdater?.start({
     manifestUrl: "../release.json",
-    workerUrl: "./service-worker.js?v=1.0.492",
+    workerUrl: "./service-worker.js?v=1.0.493",
     remoteAppUrl: memberPortal?.ok ? memberPortal.url : "",
   });
 }
